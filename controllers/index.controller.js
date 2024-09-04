6b284807c50f0b6f65908998b237b67017a63ec8
Joshtri <stuffofyos1516@gmail.com>
2024-09-04 14:14:55 +0800

    update auto generate

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -3,23 +3,12 @@
 /* eslint-disable no-useless-catch */
 // import { login } from "../services/login.services.js";
 
+import User from "../models/user.model.js";
+import bcrypt from 'bcrypt';
+
 // controllers/userController.js
 import { findUserByUsernameAndPassword, registerUser } from '../services/login.services.js';
 
-// export const loginUser = async (req, res) => {
-//     const { username, password } = req.body;
-//     try {
-//         const user = await login(username, password);
-//         // Jika login berhasil, Anda dapat mengirimkan respons yang sesuai ke klien
-//         // res.status(200).json({ message: "Login successful", user });
-        
-//         // Simpan informasi pengguna dalam sesi
-//         req.session.user = user;
-//         res.redirect('/main/dashboard');
-//     } catch (error) {
-//         res.status(401).json({ message: "Login failed", error: error.message });
-//     }
-// };
 
 
 export const loginUser = async (req, res) => {
@@ -59,18 +48,39 @@ export const registerUserController = async (req, res) => {
 
 
 
-export const loginPage = async (req,res)=>{
-    const title = "Login Page"
+
+
+export const loginPage = async (req, res) => {
+    const title = "Login Page";
     try {
-        const messageProtect = await req.flash('messageProtect')
-        res.render('index',{
+        // Mengecek apakah data users kosong
+        const usersCount = await User.countDocuments();
+        if (usersCount === 0) {
+            // Membuat username dan password secara otomatis
+            const nama_lengkap ="defaultuser";
+            const username = "defaultUser";
+            const password = "defaultPassword";
+
+            // Hash password menggunakan bcrypt
+            const saltRounds = 10;
+            const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+            // Simpan user ke dalam database
+            const newUser = new User({ username, password: hashedPassword, nama_lengkap });
+            await newUser.save();
+            
+            console.log(`User created with username: ${username} and password: ${password}`);
+        } else {
+            console.log("User data already exists. No new user created.");
+        }
+
+        const messageProtect = await req.flash('messageProtect');
+        res.render('index', {
             title,
             messageProtect
         });
     } catch (error) {
         console.log(error);
+        res.status(500).send("Server Error");
     }
-
-
-
 };
